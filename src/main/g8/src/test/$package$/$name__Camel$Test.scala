ce513833c519a12fa1fccab1373b573c0ca1ccab
Javier Ferrer González <javier.ferrer@codely.com>
2024-06-20 12:00:02 +0200

    test: upgrade to new ScalaTest packages structure

--- src/main/g8/src/test/$package$/$name__Camel$Test.scala
+++ src/main/g8/src/test/$package$/$name__Camel$Test.scala
@@ -1,21 +1,15 @@
 package $package$
 
-import org.scalatest._
-import org.scalatest.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should._
 
-final class $name;format="Camel"$Test extends WordSpec with GivenWhenThen {
+final class $name;format="Camel"$Test extends AnyWordSpec with Matchers {
   "$name;format="Camel"$" should {
     "greet" in {
-      Given("a $name;format="Camel"$")
-
       val $name;format="camel"$ = new $name;format="Camel"$
 
-      When("we ask him to greet someone")
-
-      val nameToGreet = "CodelyTV"
+      val nameToGreet = "Codely"
       val greeting = $name;format="camel"$.greet(nameToGreet)
-
-      Then("it should say hello to someone")
 
       greeting shouldBe "Hello " + nameToGreet
     }
